7692a9aea2db1fc8c2f223a3cddacbcf43eb3c21
otboss <otsurfer6@gmail.com>
2020-06-22 18:19:17 -0500

    improvements to Query.ts

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -6,24 +6,29 @@ import { IceCream } from '../misc/IceCream';
 
 export const Query = {
     async iceCreamById(parent, args, ctx, info) {
-        return prisma.iceCream({
-            id: args.id
-        });
+        return await prisma.iceCream(args);
     },
 
     async iceCreams(parent, args, ctx, info) {
-        return prisma.iceCreams();
+        return await prisma.iceCreams(args);
     },
 
     async toppings(parent, args, ctx, info) {
-        return prisma.toppingses();
+        return await prisma.toppingses(args);
     },
 
     async users(parent, args, ctx, info) {
-        return prisma.users();
+        return await prisma.users(args);
     },
 
     async cartItems(parent, args, ctx, info) {
-        return prisma.cartItems();
+        return await prisma.cartItems(args);
+    },
+
+    async iceCreamConnection(parent, args, ctx, info) {
+        const aggregate = await prisma.iceCreamsConnection(args).aggregate();
+        const pageInfo = await prisma.iceCreamsConnection().pageInfo();
+        const edges = await prisma.iceCreamsConnection().edges();
+        return { aggregate, pageInfo, edges };
     }
 }
